cec10348c89633afb568e63a6a4aafe7699d38dc
deathberg <deathberg.pro@gmail.com>
2026-06-17 00:53:23 +0300

    fix3

--- jni/hook.cpp
+++ jni/hook.cpp
@@ -1,24 +1,7 @@
-#include <jni.h>
 #include <android/log.h>
-#include "shadowhook.h"
+#define LOG_TAG "IcecamHook"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
-#define LOG_TAG "ICECAM"
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
-
-static void* orig_takePicture = nullptr;
-
-static void my_takePicture(void* instance, int msgType) {
-    LOGD("ICECAM: ХУК СРАБОТАЛ! Перехвачен takePicture");
-    // Вызов оригинала, чтобы не сломать логику камеры
-    ((void (*)(void*, int))orig_takePicture)(instance, msgType);
-}
-
-extern "C" JNIEXPORT void JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
-    LOGD("ICECAM: Загрузка модуля...");
-    
-    // Хукаем takePicture
-    shadowhook_hook_sym_name("libcamera_client.so", 
-                             "_ZN7android6Camera11takePictureEi", 
-                             (void*)my_takePicture, 
-                             (void**)&orig_takePicture);
+extern "C" void icecam_init() {
+    LOGI("Icecam hook initialized!");
 }
