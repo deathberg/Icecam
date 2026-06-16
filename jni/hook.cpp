18cac480d492788af29cf9fa6bfbd8e99151e743
deathberg <deathberg.pro@gmail.com>
2026-06-17 01:46:25 +0300

    fix13

--- jni/hook.cpp
+++ jni/hook.cpp
@@ -1,7 +1,7 @@
 #include <android/log.h>
-#define LOG_TAG "IcecamHook"
+#define LOG_TAG "IceHook"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
 extern "C" void icecam_init() {
-    LOGI("Icecam hook initialized!");
+    LOGI("Ice hook initialized!");
 }
